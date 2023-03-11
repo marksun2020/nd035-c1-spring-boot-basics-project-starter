65a76baca1120b73ed5200901d5a655337e8ec1f
WEIM011 <alexander.weimann@arvato-scs.com>
2023-03-11 19:11:41 +0100

    Functionality is implemented. No tests!

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/UserCredential.java
@@ -0,0 +1,13 @@
+package com.udacity.jwdnd.course1.cloudstorage.model;
+
+public class Credential {
+    private Integer credentialId;
+    private String username;
+    private String salt;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private Integer userId;
+
+
+}
